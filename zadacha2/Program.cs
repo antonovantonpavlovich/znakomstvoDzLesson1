f2908d3af0bed654401f26032a5d3a78fa58b123
Anton <9264622898@mail.ru>
2022-10-25 19:33:44 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите первое число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("a ="+ a + "; b ="+ b +" -> max = " +a);
+}
+if (a < b)
+{
+    Console.WriteLine("a ="+ a + "; b = "+ b +" -> max = " +b);
+}
+else
+{
+    Console.WriteLine("a ="+ a + "; b = "+ b +" числа равны");
+}
+
+//Console.WriteLine("Введите положительное 3х значное число");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int lastDigit = 0;
+//if (100 <= n && n <= 999) 
+//{
+//lastDigit = n % 10;
+//Console.WriteLine(lastDigit);
+//}
+//else 
+//{
+//    Console.WriteLine("Вы ввели неверное число");
+//}
+
+//int a = 4;
+//int b = -3;
+//int c = -7;
+//
+//Console.WriteLine (a*a);
+//Console.WriteLine (b*b);
+//Console.WriteLine (c*c);
