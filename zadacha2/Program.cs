a18daa841392bea2a1127bd916a906b9fb167dc6
Anton <9264622898@mail.ru>
2022-10-26 18:23:07 +0300

    end

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -15,23 +15,3 @@ else
     Console.WriteLine("a = "+ a + "; b = "+ b +" числа равны");
 }
 
-//Console.WriteLine("Введите положительное 3х значное число");
-//int n = Convert.ToInt32(Console.ReadLine());
-//int lastDigit = 0;
-//if (100 <= n && n <= 999) 
-//{
-//lastDigit = n % 10;
-//Console.WriteLine(lastDigit);
-//}
-//else 
-//{
-//    Console.WriteLine("Вы ввели неверное число");
-//}
-
-//int a = 4;
-//int b = -3;
-//int c = -7;
-//
-//Console.WriteLine (a*a);
-//Console.WriteLine (b*b);
-//Console.WriteLine (c*c);
