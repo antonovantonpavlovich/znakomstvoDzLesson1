f621b8f658f6c8e0f410e69060c4709ee4201f3c
Anton <9264622898@mail.ru>
2022-10-26 18:19:25 +0300

    zadacha8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a>0)
+{
+    for (int i=1; i<=a; i++)
+{
+    if (i%2==0)
+    {
+        Console.Write(i + " ");
+    }
+
+}
+}
+else
+{
+    Console.Write("четных чисел нет");
+}
+
+
